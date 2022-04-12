a465b88bd7a9a9fbe82e8c54ce8c1c9af7ba9d25
Paulo Roberto <paulovilarjnr@gmail.com>
2022-04-12 20:11:24 -0300

    first commit

---
+++ src/main/java/com/megacorp/megafood/MegafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.megacorp.megafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megacorp/megafood/MegafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megacorp.megafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
